119bb7cd2db1eb1cf3f419221581d69d5f8b76fa
pershuta001 <pershuta.p@gmail.com>
2019-06-09 16:28:35 +0300

    add import Animation to class Road

--- roadCarnage/src/gameObjects/Road.java
+++ roadCarnage/src/gameObjects/Road.java
@@ -1,9 +1,22 @@
 package gameObjects;
 
-import org.newdawn.slick.Image;
+import org.newdawn.slick.*;
+
 
 public class Road extends StaticObject {
     public Road(Image image, float scale, int x, int y, MovingObject player) {
         super(image, scale, x, y, player);
+        createAnimation();
+
+    }
+
+    private void createAnimation() {
+        Animation roadAnimation ;
+        Image roadImage;
+        try {
+             roadImage  = new Image("");
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
