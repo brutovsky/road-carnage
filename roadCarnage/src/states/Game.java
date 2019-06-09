70584b14ec8506fc2964ef3315a34a000af2d22f
Vadym <vadim.nakitnyak@gmail.com>
2019-06-09 13:51:45 +0300

    Add main class Game and state Gameplay

---
+++ roadCarnage/src/states/Game.java
@@ -0,0 +1,32 @@
+package states;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Game extends StateBasedGame{
+
+    public static final String GAME_NAME = "Road Carnage";
+    public static final int GAMEPLAY_STATE = 0;
+
+    public static void main(String[] args) {
+        Game game = new Game(GAME_NAME);
+        AppGameContainer app = null;
+        try {
+            app = new AppGameContainer(game);
+            app.setDisplayMode(1000,700,false);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Game(String gameName){
+        super(gameName);
+        addState(new Gameplay(GAMEPLAY_STATE));
+    }
+
+    @Override
+    public void initStatesList(GameContainer gameContainer) throws SlickException {
+        getState(0).init(gameContainer,this);
+    }
+}
