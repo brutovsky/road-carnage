2d6b282d21340c1e90e5d9bba1790d5b143edf45
Vadym <vadim.nakitnyak@gmail.com>
2019-06-09 16:00:18 +0300

    Add some more classes

---
+++ roadCarnage/src/gameObjects/MovingObject.java
@@ -0,0 +1,9 @@
+package gameObjects;
+
+import org.newdawn.slick.Image;
+
+public class MovingObject extends GameObject{
+    public MovingObject(Image image, float scale, int x, int y) {
+        super(image, scale, x, y);
+    }
+}
---
+++ roadCarnage/src/gameObjects/StaticObject.java
@@ -0,0 +1,10 @@
+package gameObjects;
+
+import org.newdawn.slick.Image;
+
+public class StaticObject extends GameObject {
+
+    public StaticObject(Image image, float scale, int x, int y,Player player) {
+        super(image, scale, x, y);
+    }
+}
