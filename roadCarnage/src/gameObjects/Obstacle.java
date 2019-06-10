ac7241f21af45e8705acf7e155cdd1eec9da3c90
Vadym <vadim.nakitnyak@gmail.com>
2019-06-10 14:41:22 +0300

    dessert picture renamed to duna, Obstacle and Obstacles class modified

--- roadCarnage/src/gameObjects/Obstacle.java
+++ roadCarnage/src/gameObjects/Obstacle.java
@@ -5,6 +5,9 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
 
 public class Obstacle extends GameObject {
+    public static final int ONE_LINE_SIZE = 1;
+    public static final int TWO_LINES_SIZE = 2;
+    public static final int THREE_LINES_SIZE = 3;
 
     public Obstacle(float scale, float x, float y, Obstacles obstacle) {
         super(obstacle.getAnimation(), scale, x, y);
