50b4ab3758dad7eee749f065e6a970a4b7b50584
pershuta001 <pershuta.p@gmail.com>
2019-06-12 15:13:51 +0300

    scale photos

--- roadCarnage/src/gameObjects/stuff/PlayerCars.java
+++ roadCarnage/src/gameObjects/stuff/PlayerCars.java
@@ -5,7 +5,7 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public enum PlayerCars {
-    ANISTON("res/playerCars/playerCar", "Aniston", 1f, 5, 1f, 0);
+    ANISTON("res/playerCars/playerCar", "Aniston", 5f, 5, 5f, 0);
     private Image image;
     private float speed;
     private int durability;
